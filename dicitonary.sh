3051f409e82eb0dc442153a956cfb361d1de9262
Mayank Singh <smayank748@gmail.com>
2022-04-05 09:26:30 +0530

    [Mayank]worked with dictionary

--- dicitonary.sh
+++ dicitonary.sh
@@ -1,19 +1,34 @@
 #! /bin/bash -x
 declare -A dyDictionary
 isContinue='true'
+secondIsContinue='true'
+
+function killTheLoop(){
+	local message=$1
+	read -p "$message: " userResponse
+	userResponse="${userResponse,,}"
+	if [ $userResponse != 'y' ]
+	then
+		echo false
+	else
+		echo true
+	fi
+}
 
 while [ $isContinue == 'true' ]
 do
 	read -p "Enter the key " key
 	read -p "Enter the value for the key which you have provided " value
 	dyDictionary[$key]=$value
-	read -p "press y or Y to continue " userResponse
-	userResponse="${userResponse,,}"
-	if [ $userResponse != 'y' ]
-	then
-		isContinue=false
-	fi
+	isContinue=`killTheLoop "Press y to add more value"`
 done
+
+while [ $secondIsContinue == 'true' ]
+do
+ echo "while loop is runing"
+ secondIsContinue=`killTheLoop "Do you want to continue running of loop.If yes then press y"`
+done
+
 echo ${dyDictionary[@]}
 echo ${!dyDictionary[@]}
 
