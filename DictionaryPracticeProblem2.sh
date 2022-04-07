4ccbb7597101617a24ad873c87c5b220a99513f8
Mayank Singh <smayank748@gmail.com>
2022-04-07 09:20:00 +0530

    [Mayank]solved the problem statemernt 2 with shell script

--- DictionaryPracticeProblem2.sh
+++ DictionaryPracticeProblem2.sh
@@ -1,7 +1,5 @@
-#! /bin/bash -x
 declare -A birthDate
 declare -a monthArray
-declare -a printValueAr
 NUMBER_OF_PEOPLE=50
 
 #Months between 92 and 93 are total 24 months but since
@@ -9,6 +7,7 @@ NUMBER_OF_PEOPLE=50
 TOTAL_MONTH_IN_YEAR=12
 
 numberOfPeopleInSameMonth=0;
+monthValue='';
 
 monthArray[0]='Jan'
 monthArray[1]='Feb'
@@ -25,18 +24,20 @@ monthArray[11]='December'
 
 while [ $NUMBER_OF_PEOPLE -gt 0 ]
 do
-randomMonth=$((RANDOM%$TOTAL_MONTH_IN_YEAR))
-echo $randomMonth
+	randomMonth=$((RANDOM%$TOTAL_MONTH_IN_YEAR))
+	echo $randomMonth
 
-numberOfPeopleInSameMonth=birthDate[${monthArray[$randomMonth]}]
-birthDate[${monthArray[$randomMonth]}]=$(($numberOfPeopleInSameMonth+1))
-
-printValueAr[$randomMonth]="{"${monthArray[$randomMonth]":"$numberOfPeopleInSameMonth"}"
+	monthValue=${monthArray[$randomMonth]}
+	numberOfPeopleInSameMonth=`echo ${birthDate[$monthValue]} | awk -F: '{ print $2 }'`
+	numberOfPeopleInSameMonth=`echo $numberOfPeopleInSameMonth | awk -F} '{ print $1 }'`
+	echo $numberOfPeopleInSameMonth
+	numberOfPeopleInSameMonth=$(($numberOfPeopleInSameMonth+1))
+	echo '{'$monthValue ':' $numberOfPeopleInSameMonth'}'
+	birthDate[$monthValue]=`echo '{'$monthValue ':' $numberOfPeopleInSameMonth'}'`
 
 ((NUMBER_OF_PEOPLE--))
 done
 
 echo ${birthDate[@]}
-echo ${!birthDate[@]}
-echo ${#birthDate[@]}
-echo ${printValueAr[@]}
+#echo ${!birthDate[@]}
+#echo ${#birthDate[@]}
